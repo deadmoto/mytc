e68a09a9f6292de4979e6a789ce143b1818c609f
deadmoto@gmail.com <deadmoto@gmail.com@691c9988-154c-0410-8b1e-7125a08edc26>
2008-06-01 09:00:46 +0000

    Moving icons, testing light system tray

--- trunk/mytcp.dpr
+++ trunk/mytcp.dpr
@@ -1,13 +1,19 @@
 program mytcp;
 
 uses
+  Messages,
+  ShellAPI,
+  SysUtils,
   Forms,
   Windows,
   mytc in 'mytc.pas' {Main};
 
 {$R *.res}
+const WM_ICONTRAY = WM_USER + 1;
+
 var
   HM: THandle;
+  TrayIconData: TNotifyIconData;
 
 function Check: boolean;
 begin
@@ -17,8 +23,38 @@ begin
     HM := CreateMutex(nil, false, 'MyOwnMutex');
 end;
 
+
+
+function TrayIconAdd: boolean;
+begin
+with TrayIconData do
+  begin
+  cbSize := SizeOf(TrayIconData);
+  Wnd := Application.Handle;
+  uID := 0;
+  uFlags := NIF_MESSAGE + NIF_ICON + NIF_TIP;
+  uCallbackMessage := WM_ICONTRAY;
+  hIcon := Application.Icon.Handle;
+  StrPCopy(szTip, Application.Title);
+end;
+Shell_NotifyIcon(NIM_ADD, @TrayIconData);
+end;
+
+function TrayIconMod: boolean;
+begin
+Shell_NotifyIcon(NIM_MODIFY, @TrayIconData);
+end;
+
+function TrayIconDel: boolean;
+begin
+Shell_NotifyIcon(NIM_DELETE, @TrayIconData);
+end;
+
+
 begin
   if Check then Exit;
+  if TrayIconAdd then
+  
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TMain, Main);
