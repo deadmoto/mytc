ff8f1d54f403e176c55d1b71a7461cb49616837a
deadmoto <deadmoto@691c9988-154c-0410-8b1e-7125a08edc26>
2009-12-20 17:15:33 +0000

    fixed bug when CloseNagScreen detects closing window

--- trunk/NagScreen.pas
+++ trunk/NagScreen.pas
@@ -13,6 +13,7 @@ implementation
 procedure CloseNagScreen;
 var
   NastyNagScreen: HWND;
+  LastNagScreen: HWND;
   Notebook: HWND;
   Page: HWND;
   Panel: HWND;
@@ -29,7 +30,7 @@ begin
     NumPanel := 0;
     NumButton := 0;
     NumText := '';
-    while NastyNagScreen = 0 do
+    while (NastyNagScreen = 0) or (NastyNagScreen = LastNagScreen) do
     begin
       NastyNagScreen := FindWindow('TNASTYNAGSCREEN', nil);
       Sleep(1);
@@ -37,32 +38,27 @@ begin
     while Notebook = 0 do
     begin
       Notebook := FindWindowEx(NastyNagScreen, 0, 'TNotebook', nil);
-      Sleep(1);
     end;
     while Page = 0 do
     begin
       Page := FindWindowEx(Notebook, 0, 'TPage', nil);
-      Sleep(1);
     end;
     while Panel = 0 do
     begin
       Panel := FindWindowEx(Page, 0, 'TPanel', nil);
-      Sleep(1);
     end;
     while NumPanel = 0 do
     begin
       NumPanel := FindWindowEx(Panel, 0, 'TPanel', nil);
-      Sleep(1);
     end;
     while NumButton = 0 do
     begin
       SetLength(NumText, SendMessage(NumPanel, WM_GETTEXTLENGTH, 0, 0));
       SendMessage(NumPanel, WM_GETTEXT, Length(NumText) + 1, LPARAM(NumText));
       NumButton := FindWindowEx(Panel, 0, 'TButton', PChar('&' + NumText));
-      Sleep(1);
     end;
     SendMessage(NumButton, BM_CLICK, 0, 0);
-    Sleep(1000);
+    LastNagScreen := NastyNagScreen;
   end;
 end;
 
